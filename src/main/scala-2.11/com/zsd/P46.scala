ba3bb7aa77818746383bab7c7822e4eab61c4b55
Zsolt Donca <dnczsolt@gmail.com>
2015-10-06 11:44:29 +0300

    P46 - Truth tables for logical expressions

---
+++ src/main/scala-2.11/com/zsd/P46.scala
@@ -0,0 +1,29 @@
+package com.zsd
+
+object P46 extends App {
+
+  def and(a: Boolean, b: Boolean) = a && b
+
+  def or(a: Boolean, b: Boolean) = a || b
+
+  def nand(a: Boolean, b: Boolean) = !(a && b)
+
+  def nor(a: Boolean, b: Boolean) = !(a || b)
+
+  def xor(a: Boolean, b: Boolean) = a ^ b
+
+  def impl(a: Boolean, b: Boolean) = !a || b
+
+  def equ(a: Boolean, b: Boolean) = a == b
+
+  def not(a: Boolean) = !a
+
+  def table2(f: (Boolean, Boolean) => Boolean) = {
+    println("A     B     result")
+    for (a <- Seq(true, false); b <- Seq(true, false)) {
+      println(f"$a%-5s $b%-5s ${f(a, b)}")
+    }
+  }
+
+  table2((a: Boolean, b: Boolean) => and(a, or(a, b)))
+}

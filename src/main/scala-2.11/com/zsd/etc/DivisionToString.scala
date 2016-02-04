01f0c54b86c1b709158e17382b607fb00715e5b3
Zsolt Donca <dnczsolt@gmail.com>
2016-02-04 21:31:43 +0200

    Refactoring the solution to use tail-recursion and immutability.

--- src/main/scala-2.11/com/zsd/etc/DivisionToString.scala
+++ src/main/scala-2.11/com/zsd/etc/DivisionToString.scala
@@ -1,54 +1,47 @@
 package com.zsd.etc
 
-import scala.collection.mutable
-import scala.collection.mutable.ArrayBuffer
+import java.lang.Math._
+
+import scala.annotation.tailrec
 
 object DivisionToString {
 
   def divide(dividend: Int, divisor: Int): String = {
+    if (divisor == 0) {
+      "NaN"
+    } else {
+      val negative = (dividend < 0) != (divisor < 0)
+      val resultSeq = (if (negative) Seq("-") else Seq.empty) ++ dividePositive(abs(dividend), abs(divisor))
+      resultSeq.mkString
+    }
+  }
 
-    if (divisor == 0) return "NaN"
-
-    val isNegative = (dividend < 0) != (divisor < 0)
-    val history = mutable.Map(0 -> -1)
-    val results = ArrayBuffer[Any]()
-    var distance = 1
-
-    import Math._
-    for (step <- getSteps(abs(dividend), abs(divisor))) {
-      results += step.ratio
-
-      def previousStep = history.get(step.next)
-      if (previousStep.isEmpty) {
-        history(step.next) = distance
-        distance += 1
-      }
-      else {
-        return toString(results, previousStep.get, isNegative)
+  def dividePositive(dividend: Int, divisor: Int): Seq[Any] = {
+    @tailrec
+    def divide(steps: Stream[DivisionStep], distance: Int, history: Map[Int, Int], previousResult: List[Int]): Seq[Any] = {
+      val (step #:: restOfSteps) = steps
+      val result = step.ratio :: previousResult
+      history.get(step.next) match {
+        case Some(repetitionStart) => insertPunctuation(result.reverse.toSeq, repetitionStart)
+        case None => divide(restOfSteps, distance + 1, history.updated(step.next, distance), result)
       }
     }
 
-    throw new IllegalStateException
+    val steps = Stream.iterate(new DivisionStep(dividend, divisor))(current => new DivisionStep(current.next, divisor))
+    divide(steps, 1, Map(0 -> -1), List.empty)
   }
 
-  def getSteps(dividend: Int, divisor: Int): Stream[DivisionStep] = {
-    Stream.iterate(new DivisionStep(dividend, divisor)) {
-      current => new DivisionStep(current.next, divisor)
+  def insertPunctuation(results: Seq[Int], repStart: Int): Seq[Any] = {
+    if (results.length == 1) {
+      results.toSeq
+    } else if (repStart >= 0) {
+      Seq(results.head, ".") ++ results.slice(1, repStart) ++ Seq("(") ++ results.slice(repStart, results.length) ++ Seq(")")
+    } else {
+      Seq(results.head, ".") ++ results.slice(1, results.length)
     }
   }
 
-  def toString(results: ArrayBuffer[Any], repetitionStart: Int, negative: Boolean) = {
-    if (repetitionStart >= 0) {
-      results.insert(repetitionStart, "(")
-      results += ')'
-    }
-
-    if (results.length > 1) results.insert(1, ".")
-    if (negative) results.insert(0, "-")
-    results.mkString
-  }
-
-  case class DivisionStep(dividend: Int, divisor: Int) {
+  class DivisionStep(dividend: Int, divisor: Int) {
     val ratio = dividend / divisor
     val next = (dividend % divisor) * 10
   }
--- src/test/scala-2.11/com/zsd/etc/DivisionToStringTest.scala
+++ src/test/scala-2.11/com/zsd/etc/DivisionToStringTest.scala
@@ -17,20 +17,20 @@ class DivisionToStringTest extends FlatSpec with Matchers {
 
   val divisionExamples = Table(
     ("dividend", "divisor", "result"),
-    (1        , 0       , "NaN"),        // illegal division
-    (10       , 2       , "5"),          // no fraction
-    (1        , 2       , "0.5"),        // no repetition
-    (1        , 3       , "0.(3)"),      // simple repetition
-    (22       , 7       , "3.(142857)"), // longer repeating part
-    (11       , 6       , "1.8(3)"),     // non-repeating fractional part
+    (1         , 0        , "NaN"),        // illegal division
+    (10        , 2        , "5"),          // no fraction
+    (1         , 2        , "0.5"),        // no repetition
+    (1         , 3        , "0.(3)"),      // simple repetition
+    (22        , 7        , "3.(142857)"), // longer repeating part
+    (11        , 6        , "1.8(3)"),     // non-repeating fractional part
 
-    (-1       ,  4      , "-0.25"),      // negative divident
-    ( 1       , -3      , "-0.(3)"),     // negative divisor
-    (-1       , -7      , "0.(142857)"), // negative both
+    (-1        ,  4       , "-0.25"),      // negative divident
+    ( 1        , -3       , "-0.(3)"),     // negative divisor
+    (-1        , -7       , "0.(142857)"), // negative both
 
-    (5        , 74      , "0.0(675)"),
-    (1        , 28      , "0.03(571428)"),
-    (1        , 24      , "0.041(6)"),
-    (1        , 29      , "0.(0344827586206896551724137931)")
+    (5         , 74       , "0.0(675)"),
+    (1         , 28       , "0.03(571428)"),
+    (1         , 24       , "0.041(6)"),
+    (1         , 29       , "0.(0344827586206896551724137931)")
   )
 }

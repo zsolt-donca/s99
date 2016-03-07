1860de7392e7a0117f8ef4d87e17f4938ca69d53
Pap Lőrinc <paplorinc@yahoo.com>
2016-03-07 20:39:19 +0200

    Style simplifications for the `WaterBottle` exercise

--- src/main/scala-2.11/com/zsd/etc/WaterBottle.scala
+++ src/main/scala-2.11/com/zsd/etc/WaterBottle.scala
@@ -3,68 +3,45 @@ package com.zsd.etc
 import java.lang.Math._
 
 object WaterBottle extends App {
-
-  object Op extends Enumeration {
-    val Pour, Fill, Empty = Value
-  }
-
-  object Side extends Enumeration {
-    val Left, Right = Value
-  }
+  object Op extends Enumeration { val Pour, Fill, Empty = Value } 
+  object Side extends Enumeration { val Left, Right = Value }
 
   case class Action(op: Op.Value, side: Side.Value)
+  case class State(left: Int, right: Int) { def swap = State(right, left) } 
+  case class Capacity(left: Int, right: Int) { def swap = Capacity(right, left) }
 
-  case class State(left: Int, right: Int) {
-    def flip = State(right, left)
-  }
-
-  case class Capacity(left: Int, right: Int) {
-    def flip = Capacity(right, left)
-  }
-
-  def applyAction(state: State, capacity: Capacity, action: Action): State = {
+  def applyAction(action: Action, state: State, capacity: Capacity) = {
     import Side._
     action.side match {
-      case Left =>
-        applyOpFromLeft(state, capacity, action.op)
-      case Right =>
-        applyOpFromLeft(state.flip, capacity.flip, action.op).flip
+      case Left  ⇒ applyOpFromLeft(action.op, state, capacity)
+      case Right ⇒ applyOpFromLeft(action.op, state.swap, capacity.swap).swap
     }
   }
 
-  def applyOpFromLeft(state: State, capacity: Capacity, op: Op.Value): State = {
+  def applyOpFromLeft(op: Op.Value, state: State, capacity: Capacity) = {
     import Op._
+    val sum = state.left + state.right
     op match {
-      case Pour =>
-        val sum = state.left + state.right
-        State(left = max(0, sum - capacity.right), right = min(sum, capacity.right))
-      case Fill =>
-        State(capacity.left, state.right)
-      case Empty =>
-        State(0, state.right)
+      case Pour  ⇒ State(max(0, sum - capacity.right), min(sum, capacity.right))
+      case Fill  ⇒ State(capacity.left, state.right)
+      case Empty ⇒ State(0, state.right)
     }
   }
 
-  def solve(capacity: Capacity, desired: Int): Seq[Seq[State]] = {
-    solve(State(0, 0), capacity, desired, List.empty)
-  }
+  def solve(capacity: Capacity, desired: Int, state: State = State(0, 0), seen: Seq[State] = Seq.empty): Seq[Seq[State]] = {
+    val solution = state +: seen
+    if (Seq(state.left, state.right) contains desired)
+      Seq(solution.reverse)
+    else
+      for (op   ← Op.values.toSeq;
+           side ← Side.values.toSeq;
 
-  def solve(state: State, capacity: Capacity, desired: Int, seen: List[State]): Seq[Seq[State]] = {
-    if (state.left == desired || state.right == desired) {
-      Seq((state +: seen).reverse)
-    } else {
-      for (op: Op.Value <- Seq(Op.Pour, Op.Fill, Op.Empty);
-           side: Side.Value <- Seq(Side.Left, Side.Right);
-           action = Action(op, side);
-           newState = applyAction(state, capacity, action)
+           newState = applyAction(Action(op, side), state, capacity)
            if state != newState && !seen.contains(newState);
-           results <- solve(newState, capacity, desired, state +: seen))
-        yield results
-    }
+
+           results ← solve(capacity, desired, newState, solution)
+      ) yield results
   }
 
-  def bestSolutions(solutions: Seq[Seq[State]]): Seq[Seq[State]] = {
-    val bestSize = solutions.map(_.size).min
-    solutions.filter(_.size == bestSize)
-  }
+  def bestSolution(solutions: Seq[Seq[State]]) = solutions.min(Ordering.by((_: Seq[State]).size))
 }
